e452f439ec65993cb1d8aefd2c30557df1839d38
akiring <akio@mail.com>
2025-04-14 19:02:12 +0900

    modified

--- sukunabikona-aws/lib/functions/perplexity-api.ts
+++ sukunabikona-aws/lib/functions/perplexity-api.ts
@@ -1,5 +1,35 @@
-import { APIGatewayProxyHandlerV2 } from "aws-lambda";
+import { Handler } from "aws-lambda";
+import { URLSearchParams } from "url";
 
-export const handler: APIGatewayProxyHandlerV2 = async (event) => {
-    return {};
+const paramsAndSecretsUrl = 'http://localhost:2773/systemsmanager/parameters/get';
+const perplexityApiEndpoint = 'https://api.perplexity.ai/chat/completions';
+
+export const handler: Handler = async (event) => {
+
+    const apiKey = await fetch(`${paramsAndSecretsUrl}?${new URLSearchParams({
+        name: '/perplexity/apikey',
+        withDecryption: 'true',
+    })}`, {
+        method: 'GET',
+        headers: {
+            'X-Aws-Parameters-Secrets-Token': process.env.AWS_SESSION_TOKEN || '',
+        },
+    })
+        .then(response => response.json())
+        .then(json => json.Parameter.Value);
+
+    const answer = await fetch(`${perplexityApiEndpoint}`, {
+        method: 'GET',
+        headers: {
+            'accept': 'application/json',
+            'content-type': 'application/json',
+            'Authorization:': `Bearer ${apiKey}`,
+        },
+        body: JSON.stringify({
+            model: 'sonar-pro',
+            messages: [],
+        })
+    });
+
+    return { apiKey, };
 }
--- sukunabikona-aws/lib/sukunabikona-aws-stack.ts
+++ sukunabikona-aws/lib/sukunabikona-aws-stack.ts
@@ -1,8 +1,32 @@
-import { Stack, StackProps } from "aws-cdk-lib";
+import { CfnOutput, Stack, StackProps } from "aws-cdk-lib";
+import { ParamsAndSecretsLayerVersion, ParamsAndSecretsVersions } from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { StringParameter } from "aws-cdk-lib/aws-ssm";
 import { Construct } from "constructs";
 
+const paramsAndSecretsArns = {
+    'ap-northeast-1': 'arn:aws:lambda:ap-northeast-1:133490724326:layer:AWS-Parameters-and-Secrets-Lambda-Extension:12',
+} as { [key: string]: string };
+
 export class SukunabikonaAwsStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
-  }
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);
+
+        const region = props?.env?.region || '';
+        const paramsAndSecrets = ParamsAndSecretsLayerVersion.fromVersionArn(paramsAndSecretsArns[region]);
+        if (paramsAndSecrets == undefined) {
+            throw new Error('The required values are missing.');
+        }
+
+        const apiKey = StringParameter.fromSecureStringParameterAttributes(this, 'ApiKey', {
+            parameterName: '/perplexity/apikey',
+        });
+        const perplexityApi = new NodejsFunction(this, 'PerplexityApi', {
+            entry: `${__dirname}/functions/perplexity-api.ts`,
+            paramsAndSecrets: paramsAndSecrets,
+        });
+        apiKey.grantRead(perplexityApi);
+
+        new CfnOutput(this, 'PerplexityApiFunctionName', { value: perplexityApi.functionName });
+    }
 }
